fe5dba73b2a71907f3fb8500a848fff972a22b08
Jaeyong Lee <jaeyonglee@geotab.com>
2024-10-14 14:53:16 -0400

    initlaize firebase only if instance doesnt exist

--- frontend/src/config/firebaseConfig.ts
+++ frontend/src/config/firebaseConfig.ts
@@ -1,10 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApp, initializeApp } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,8 +19,15 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const getFirebaseApp = () => {
+    try {
+        return getApp();
+    } catch (err) {
+        return initializeApp(firebaseConfig);
+    }
+}
+
+const app = getFirebaseApp();
 // const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
