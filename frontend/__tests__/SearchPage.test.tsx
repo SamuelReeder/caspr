735cf21898d7344787eb5e25de794d3d1a495c80
kev0921 <kevin.hu0921@gmail.com>
2024-10-14 16:41:05 -0400

    added back searchpage tests

--- frontend/__tests__/SearchPage.test.tsx
+++ frontend/__tests__/SearchPage.test.tsx
@@ -1,11 +1,11 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import SearchPage from './SearchPage';
-// import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SearchPage from '../src/pages/SearchPage';
+import '@testing-library/jest-dom';
 
 test('renders SearchPage component', () => {
-    // render(<SearchPage />);
-    // const searchPageElement = screen.getByText(/Explore/i);
-    // expect(searchPageElement).toBeInTheDocument();
+    render(<SearchPage />);
+    const searchPageElement = screen.getByText(/Explore/i);
+    expect(searchPageElement).toBeInTheDocument();
     expect(true).toBe(true);
 });
