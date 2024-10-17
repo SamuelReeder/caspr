c0de18b7231961c6fee4b3e7e6b13ece3239a4d3
Jaeyong Lee <jaeyonglee@geotab.com>
2024-10-16 21:46:20 -0400

    delete reset pword page, add routes to slug

--- frontend/src/pages/[...slug].tsx
+++ frontend/src/pages/[...slug].tsx
@@ -1,14 +1,24 @@
-import CreateAccount from './createAccount';
-import { useRouter } from 'next/router';
+import CreateAccount from "./createAccount";
+import ForgotPassword from "./forgotPassword";
+import UploadFile from "./uploadFile";
+import { useRouter } from "next/router";
 
 export default function CatchAllRoute() {
-  const router = useRouter();
-  const { slug } = router.query;
+	const router = useRouter();
+	const { slug } = router.query;
 
-  if (slug && slug[0] === 'create-account') {
-    return <CreateAccount />;
-  }
+	if (slug && slug[0] === "create-account") {
+		return <CreateAccount />;
+	}
 
-  // TODO - display a 404 error page
-  return <div>Page not found</div>; 
+	if (slug && slug[0] === "upload-file") {
+		return <UploadFile />;
+	}
+
+	if (slug && slug[0] === "forgot-password") {
+		return <ForgotPassword />;
+	}
+
+	// TODO - display a 404 error page
+	return <div>Page not found</div>;
 }
