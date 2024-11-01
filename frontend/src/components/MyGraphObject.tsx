cd16803d8ad4a42fed144d9da93b035a085a043c
Jaeyong Lee <jaeyonglee@geotab.com>
2024-11-01 14:24:53 -0400

    merge home pages, search not added yet

--- frontend/src/components/MyGraphObject.tsx
+++ frontend/src/components/MyGraphObject.tsx
@@ -1,42 +1,46 @@
+import {
+	Box,
+	Card,
+	CardBody,
+	CardHeader,
+	Heading,
+	Text
+} from "@chakra-ui/react";
 
-import { Box, Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react';
-import React from 'react';
-import ShareButton from './ShareButton';
+import React from "react";
+import ShareButton from "./ShareButton";
 
 interface GraphObjectProps {
-    title: string;
-    description: string;
-    author: string;
-    // Add more props here
+	graphName: string;
+	graphDescription: string;
+	author: string;
 }
 
-const MyGraphObject = ({ title, description, author }: GraphObjectProps) => {
-    return (
-        <Card my='5'>
-            <CardHeader className='flex justify-between'>
-                <Heading size='md'>{title}</Heading>
-                <Text>{author}</Text> 
-            </CardHeader>
+const MyGraphObject = ({
+	graphName,
+	graphDescription,
+	author
+}: GraphObjectProps) => {
+	return (
+		<Card my="5">
+			<CardHeader className="flex justify-between">
+				<Heading size="md">{graphName}</Heading>
+				<Text>{author}</Text>
+			</CardHeader>
 
-            <CardBody className='flex justify-between'>
-                <Box>
-                    <Heading size='xs' textTransform='uppercase'>
-                        Description:
-                    </Heading>
-                    <Text pt='1' pr='1' fontSize='sm'>
-                        {description}
-                    </Text>
-                </Box>
-                <ShareButton url={''} title={title}></ShareButton>
-            </CardBody>
-        </Card>
-    );
+			<CardBody className="flex justify-between">
+				<Box>
+					<Heading size="xs" textTransform="uppercase">
+						graphDescription:
+					</Heading>
+					<Text pt="1" pr="1" fontSize="sm">
+						{graphDescription}
+					</Text>
+				</Box>
+				{/* <ShareButton url={""} graphName={graphName}></ShareButton> */}
+			</CardBody>
+		</Card>
+	);
 };
 
 export default MyGraphObject;
-
-
-// <div key={index} className="bg-gray-100 p-4 rounded-lg my-4">
-//   <div className="w-full h-64 bg-gray-300 mb-4"></div>
-//   <span className="text-sm font-semibold">{result.title}</span>
-// </div>
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,50 +1,66 @@
-import { Box, VStack, Text, Link } from '@chakra-ui/react';
-import { usePage } from '@/context/PageContext';
-import { useRouter } from 'next/router';
+import { Box, Button, Link, Text, VStack } from "@chakra-ui/react";
+import router, { useRouter } from "next/router";
+
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { universalLogout } from "@/api/auth";
+import { usePage } from "@/context/PageContext";
 
 export default function Sidebar() {
-  const { page, setPage } = usePage();
+	const { page, setPage } = usePage();
+	const router = useRouter();
 
-  const handlePageChange = (newPage: string) => {
-    setPage(newPage);
-  };
+	const handlePageChange = (newPage: string) => {
+		setPage(newPage);
+	};
 
-  return (
-    <Box
-      as="nav"
-      bg="gray.800"
-      color="white"
-      w="18rem"
-      p="4"
-      className="shadow-lg"
-      height="100vh"
-      position="fixed"
-      top="0"
-      left="0"
-    >
-      <VStack spacing="4" align="stretch">
-        <Text fontSize="2xl" fontWeight="bold">
-          Caspr
-        </Text>
-        <Link
-          className={`hover:text-gray-400 ${page === 'My Graphs' ? 'text-gray-400' : ''}`}
-          onClick={() => handlePageChange('My Graphs')}
-        >
-          My Graphs
-        </Link>
-        <Link
-          className={`hover:text-gray-400 ${page === 'Shared With Me' ? 'text-gray-400' : ''}`}
-          onClick={() => handlePageChange('Shared With Me')}
-        >
-          Shared With Me
-        </Link>
-        <Link
-          className={`hover:text-gray-400 ${page === 'Explore' ? 'text-gray-400' : ''}`}
-          onClick={() => handlePageChange('Explore')}
-        >
-          Explore
-        </Link>
-      </VStack>
-    </Box>
-  );
+	const handleLogout = async () => {
+		try {
+			await universalLogout();
+			router.push("/");
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<div className="bg-gray-800 text-white w-60 p-4 shadow-lg top-0 left-0 h-full">
+			<VStack spacing="4" align="stretch">
+				<Text fontSize="2xl" fontWeight="bold">
+					Caspr
+				</Text>
+				<div className="flex flex-row gap-4 justify-center items-center	">
+					<Button colorScheme="blue" onClick={handleLogout} size="sm">
+						Logout
+					</Button>
+					<Link href="/upload-file">
+						<Button
+							className="border rounded-lg p-2"
+							size="sm"
+							rightIcon={<ArrowForwardIcon />}
+						>
+							Upload a File
+						</Button>
+					</Link>
+				</div>
+				<Link
+					className={`hover:text-gray-400 ${page === "My Graphs" ? "text-gray-400" : ""}`}
+					onClick={() => handlePageChange("My Graphs")}
+				>
+					My Graphs
+				</Link>
+				<Link
+					className={`hover:text-gray-400 ${page === "Shared With Me" ? "text-gray-400" : ""}`}
+					onClick={() => handlePageChange("Shared With Me")}
+				>
+					Shared With Me
+				</Link>
+				<Link
+					className={`hover:text-gray-400 ${page === "Explore" ? "text-gray-400" : ""}`}
+					onClick={() => handlePageChange("Explore")}
+				>
+					Explore
+				</Link>
+			</VStack>
+		</div>
+	);
 }
