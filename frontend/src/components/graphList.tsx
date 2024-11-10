72b8217064eda18830514239004cd5fb50e002e3
Jaeyong Lee <jaeyonglee@geotab.com>
2024-11-10 18:33:43 -0500

    rename my graph object

--- frontend/src/components/graphList.tsx
+++ frontend/src/components/graphList.tsx
@@ -1,25 +1,28 @@
-import { Graph, GraphListProps } from "@/types/graph";
 /**
  * Graph list component
  * @param {Array} graphs - List of graphs
  * @param {String} page - Page title
  * @returns {ReactElement} Graph list component
  */
-import { Heading } from "@chakra-ui/react";
-import MyGraphObject from "./MyGraphObject";
-import { get } from "http";
-import { User } from "@/types";
+import { Box, Heading } from "@chakra-ui/react";
+import { Graph, GraphListProps } from "@/types/graph";
 import { useEffect, useState } from "react";
+
+import ExploreGraphCard from "./ExploreGraphCard";
+import MyGraphCard from "./MyGraphCard";
+import { User } from "@/types";
 import { getUser } from "@/api";
 import { useAuth } from "@/context";
 
-export function GraphList({ graphs, page }: GraphListProps) {
+const GraphList = ({ graphs, page }: GraphListProps) => {
 	const { firestoreUser } = useAuth();
 
-	const [graphsWithOwners, setGraphsWithOwners] = useState<Array<{
-		graph: Graph,
-		owner: User | null
-	}>>([]);
+	const [graphsWithOwners, setGraphsWithOwners] = useState<
+		Array<{
+			graph: Graph;
+			owner: User | null;
+		}>
+	>([]);
 
 	useEffect(() => {
 		const fetchOwners = async () => {
@@ -29,7 +32,7 @@ export function GraphList({ graphs, page }: GraphListProps) {
 				graphs.map(async (graph) => {
 					try {
 						if (graph.owner == firestoreUser?.uid) {
-							return {graph, owner: firestoreUser}
+							return { graph, owner: firestoreUser };
 						}
 						const owner = await getUser(graph.owner);
 						return { graph, owner };
@@ -43,23 +46,34 @@ export function GraphList({ graphs, page }: GraphListProps) {
 		};
 
 		fetchOwners();
-	}, [graphs]);
-
+	}, [firestoreUser, graphs]);
 
 	return (
 		<div className="mt-8">
 			<Heading size="md" mb={4}>
 				{page}
 			</Heading>
-			<div className="flex flex-col gap-4">
-				{graphsWithOwners.map(({ graph, owner }, i) => (
-					<MyGraphObject
-						key={graph.id || i}
-						graph={graph}
-						owner={owner}
-					/>
-				))}
-			</div>
+			{page === "Explore" ? (
+				<div className="flex flex-wrap flex-row gap-2">
+					{graphsWithOwners.map(({ graph, owner }, i) => (
+						<Box key={graph.id || i} className="w-[32.5%] pt-2">
+							<ExploreGraphCard
+								key={graph.id || i}
+								graph={graph}
+								owner={owner}
+							/>
+						</Box>
+					))}
+				</div>
+			) : (
+				<div className="flex flex-col gap-4">
+					{graphsWithOwners.map(({ graph, owner }, i) => (
+						<MyGraphCard key={graph.id || i} graph={graph} owner={owner} />
+					))}
+				</div>
+			)}
 		</div>
 	);
-}
+};
+
+export default GraphList;
