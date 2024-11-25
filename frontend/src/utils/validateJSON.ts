cfea1627be8f8fea58ec3d91edadf699e57e8cf4
Danny Lu <danny.lu075@gmail.com>
2024-11-25 13:55:58 -0500

    Changed logic to work with non-timestamped format json as well.

--- frontend/src/utils/validateJSON.ts
+++ frontend/src/utils/validateJSON.ts
@@ -3,50 +3,45 @@ interface ValidationResult {
 	errorMessage: string | null;
 }
 
+// Checks if the data is in the timestamp format
+const isTimestampFormat = (data: any): boolean => {
+	// Check if data has timestamps and time_unit as a key
+	const requiredKeys = ["timestamps", "time_unit"];
+	const hasRequiredKeys = requiredKeys.every((key) => key in data);
+
+	return hasRequiredKeys;
+};
+
+const isNonTimestampFormat = (data: any): boolean => {
+	const requiredKeys = ["nodes", "edges"];
+	const hasRequiredKeys = requiredKeys.every((key) => key in data);
+
+	return hasRequiredKeys;
+};
+
 export const validateJSON = (json: string): ValidationResult => {
 	try {
 		// Check if JSON syntax is valid
 		const data = JSON.parse(json);
 
-		// Validate top-level structure of graph data
-		const graphDataValidationResult = validateGraphData(data);
-		if (!graphDataValidationResult.isValid) {
-			return graphDataValidationResult;
+		if (!data || typeof data !== "object") {
+			return {
+				isValid: false,
+				errorMessage: "Invalid JSON syntax"
+			};
 		}
 
-		// Validate each timestamp
-		for (const timestamp of data.timestamps) {
-			const timestampValidationResult = validateTimestamp(timestamp);
-			if (!timestampValidationResult.isValid) {
-				return timestampValidationResult;
-			}
+		if (isTimestampFormat(data)) {
+			return validateTimestampFormat(data);
+		} else if (isNonTimestampFormat(data)) {
+			return validateNonTimestampFormat(data);
+		} else {
+			return {
+				isValid: false,
+				errorMessage:
+					"Invalid graph format, for timestamp format use keys:'timestamps' and 'time_unit', for non-timestamp format use 'nodes' and 'edges'"
+			};
 		}
-
-		// Validate each node
-		for (const timestamp of data.timestamps) {
-			for (const node of timestamp.nodes) {
-				const nodeValidationResult = validateNode(node);
-				if (!nodeValidationResult.isValid) {
-					return nodeValidationResult;
-				}
-			}
-		}
-
-		// Validate each edge
-		for (const timestamp of data.timestamps) {
-			for (const edge of timestamp.edges) {
-				const edgeValidationResult = validateEdge(edge, timestamp.nodes);
-				if (!edgeValidationResult.isValid) {
-					return edgeValidationResult;
-				}
-			}
-		}
-
-		// If passes all validation, return the data
-		return {
-			isValid: true,
-			errorMessage: null
-		};
 	} catch (error) {
 		return {
 			isValid: false,
@@ -55,155 +50,292 @@ export const validateJSON = (json: string): ValidationResult => {
 	}
 };
 
-const validateGraphData = (data: any): ValidationResult => {
-	let isValid = true;
-	let errorMessage = null;
+const validateTimestampFormat = (data: any): ValidationResult => {
+	// Validate the top-level structure of graph data
+	const graphDataValidationResult = validateGraphData(data);
+	if (!graphDataValidationResult.isValid) {
+		return graphDataValidationResult;
+	}
 
+	// Validate each timestamp
+	for (const timestamp of data.timestamps) {
+		const timestampValidationResult = validateTimestamp(timestamp);
+		if (!timestampValidationResult.isValid) {
+			return timestampValidationResult;
+		}
+	}
+
+	// Validate each node
+	for (const timestamp of data.timestamps) {
+		for (const node of timestamp.nodes) {
+			const nodeValidationResult = validateNode(node);
+			if (!nodeValidationResult.isValid) {
+				return nodeValidationResult;
+			}
+		}
+	}
+
+	// Validate each edge
+	for (const timestamp of data.timestamps) {
+		for (const edge of timestamp.edges) {
+			const edgeValidationResult = validateEdge(edge);
+			if (!edgeValidationResult.isValid) {
+				return edgeValidationResult;
+			}
+		}
+	}
+
+	return {
+		isValid: true,
+		errorMessage: null
+	};
+};
+
+const validateNonTimestampFormat = (data: any): ValidationResult => {
+	const requiredKeys = ["nodes", "edges"];
+	const hasRequiredKeys = requiredKeys.every((key) => key in data);
+
+	if (!hasRequiredKeys) {
+		return {
+			isValid: false,
+			errorMessage: "Invalid keys, must be 'nodes' and 'edges'"
+		};
+	}
+
+	// Validate Each node
+	for (const node of data.nodes) {
+		const nodeValidationResult = validateNode(node);
+		if (!nodeValidationResult.isValid) {
+			return nodeValidationResult;
+		}
+	}
+
+	// Validate each edge
+	for (const edge of data.edges) {
+		const edgeValidationResult = validateEdge(edge);
+		if (!edgeValidationResult.isValid) {
+			return edgeValidationResult;
+		}
+	}
+
+	// Validate Each edge
+	return {
+		isValid: true,
+		errorMessage: null
+	};
+};
+
+const validateGraphData = (data: any): ValidationResult => {
 	// If data is not an object or is null, it is not a valid graph
 	if (typeof data !== "object" || data === null) {
-		isValid = false;
-		errorMessage = "Invalid graph JSON format";
+		return {
+			isValid: false,
+			errorMessage: "Invalid graph JSON format"
+		};
 	}
 
 	// If time_unit is not a string, it is not a valid graph
 	if (typeof data.time_unit !== "string") {
-		isValid = false;
-		errorMessage = "Invalid time_unit, must be a string";
+		return {
+			isValid: false,
+			errorMessage: "Invalid time_unit, must be a string"
+		};
 	}
 
 	// If timestamps is not an array, it is not a valid graph
 	if (!Array.isArray(data.timestamps)) {
-		isValid = false;
-		errorMessage = "Invalid timestamps, must be an array";
+		return {
+			isValid: false,
+			errorMessage: "Invalid timestamps, must be an array"
+		};
 	}
 
 	if (!data.timestamps.length) {
-		isValid = false;
-		errorMessage = "Timestamps array cannot be empty";
+		return {
+			isValid: false,
+			errorMessage: "Timestamps array cannot be empty"
+		};
 	}
 
 	return {
-		isValid,
-		errorMessage: errorMessage
+		isValid: true,
+		errorMessage: null
 	};
 };
 
 const validateTimestamp = (timestamp: any): ValidationResult => {
-	let isValid = true;
-	let errorMessage = null;
+	const requiredKeys = ["t", "nodes", "edges"];
+	const hasRequiredKeys = requiredKeys.every((key) => key in timestamp);
+
+	if (!hasRequiredKeys) {
+		return {
+			isValid: false,
+			errorMessage: `Missing/Incorrect required keys: ${requiredKeys.filter((key) => !(key in timestamp)).join(", ")}`
+		};
+	}
 
 	if (typeof timestamp !== "object") {
-		isValid = false;
-		errorMessage = "Invalid timestamp, must be an object";
+		return {
+			isValid: false,
+			errorMessage: "Invalid timestamp, must be an object"
+		};
 	}
 
 	if (timestamp === null) {
-		isValid = false;
-		errorMessage = "Timestamp cannot be null";
+		return {
+			isValid: false,
+			errorMessage: "Timestamp cannot be null"
+		};
 	}
 
 	if (typeof timestamp.t !== "number") {
-		isValid = false;
-		errorMessage = "Invalid timestamp, must be a number";
+		return {
+			isValid: false,
+			errorMessage: "Invalid timestamp, must be a number"
+		};
 	}
 
 	if (!Array.isArray(timestamp.nodes)) {
-		isValid = false;
-		errorMessage = "Invalid timestamp, nodes must be an array";
+		return {
+			isValid: false,
+			errorMessage: "Invalid timestamp, nodes must be an array"
+		};
 	}
 
 	if (!Array.isArray(timestamp.edges)) {
-		isValid = false;
-		errorMessage = "Invalid timestamp, edges must be an array";
+		return {
+			isValid: false,
+			errorMessage: "Invalid timestamp, edges must be an array"
+		};
 	}
 
 	return {
-		isValid,
-		errorMessage: errorMessage
+		isValid: true,
+		errorMessage: null
 	};
 };
 
 const validateNode = (node: any): ValidationResult => {
-	let isValid = true;
-	let errorMessage = null;
+	const requiredKeys = ["id", "label", "value", "category"];
+	const hasRequiredKeys = requiredKeys.every((key) => key in node);
+
+	if (!hasRequiredKeys) {
+		return {
+			isValid: false,
+			errorMessage: `Missing/Incorrect required keys: ${requiredKeys.filter((key) => !(key in node)).join(", ")}`
+		};
+	}
 
 	if (typeof node !== "object") {
-		isValid = false;
-		errorMessage = "Invalid node, must be an object";
+		return {
+			isValid: false,
+			errorMessage: "Invalid node, must be an object"
+		};
 	}
 
 	if (node === null) {
-		isValid = false;
-		errorMessage = "Node cannot be null";
+		return {
+			isValid: false,
+			errorMessage: "Node cannot be null"
+		};
 	}
 
 	if (typeof node.id !== "string") {
-		isValid = false;
-		errorMessage = "Invalid node, id must be a string";
+		return {
+			isValid: false,
+			errorMessage: "Invalid node, id must be a string"
+		};
 	}
 
 	if (typeof node.label !== "string") {
-		isValid = false;
-		errorMessage = "Invalid node, label must be a string";
+		return {
+			isValid: false,
+			errorMessage: "Invalid node, label must be a string"
+		};
 	}
 
 	if (typeof node.value !== "number") {
-		isValid = false;
-		errorMessage = "Invalid node, value must be a number";
+		return {
+			isValid: false,
+			errorMessage: "Invalid node, value must be a number"
+		};
 	}
 
 	if (typeof node.category !== "string") {
-		isValid = false;
-		errorMessage = "Invalid node, category must be a string";
+		return {
+			isValid: false,
+			errorMessage: "Invalid node, category must be a string"
+		};
 	}
 
 	return {
-		isValid,
-		errorMessage: errorMessage
+		isValid: true,
+		errorMessage: null
 	};
 };
 
-const validateEdge = (edge: any, nodes: any[]): ValidationResult => {
-	let isValid = true;
-	let errorMessage = null;
+const validateEdge = (edge: any): ValidationResult => {
+	const requiredKeys = ["source", "target", "relationship", "strength"];
+	const hasRequiredKeys = requiredKeys.every((key) => key in edge);
+
+	if (!hasRequiredKeys) {
+		return {
+			isValid: false,
+			errorMessage: `Missing/Incorrect required keys: ${requiredKeys.filter((key) => !(key in edge)).join(", ")}`
+		};
+	}
 
 	if (typeof edge !== "object") {
-		isValid = false;
-		errorMessage = "Invalid edge, must be an object";
+		return {
+			isValid: false,
+			errorMessage: "Invalid edge, must be an object"
+		};
 	}
 
 	if (edge === null) {
-		isValid = false;
-		errorMessage = "Edge cannot be null";
+		return {
+			isValid: false,
+			errorMessage: "Edge cannot be null"
+		};
 	}
 
 	if (typeof edge.source !== "string") {
-		isValid = false;
-		errorMessage = "Invalid edge, source must be a string";
+		return {
+			isValid: false,
+			errorMessage: "Invalid edge, source must be a string"
+		};
 	}
 
 	if (typeof edge.target !== "string") {
-		isValid = false;
-		errorMessage = "Invalid edge, target must be a string";
+		return {
+			isValid: false,
+			errorMessage: "Invalid edge, target must be a string"
+		};
 	}
 
 	if (typeof edge.relationship !== "string") {
-		isValid = false;
-		errorMessage = "Invalid edge, relationship must be a string";
+		return {
+			isValid: false,
+			errorMessage: "Invalid edge, relationship must be a string"
+		};
 	}
 
 	if (typeof edge.strength !== "number") {
-		isValid = false;
-		errorMessage = "Invalid edge, strength must be a number";
+		return {
+			isValid: false,
+			errorMessage: "Invalid edge, strength must be a number"
+		};
 	}
 
 	if (edge.strength < 0 || edge.strength > 1) {
-		isValid = false;
-		errorMessage = "Invalid edge, strength must be between 0 and 1";
+		return {
+			isValid: false,
+			errorMessage: "Invalid edge, strength must be between 0 and 1"
+		};
 	}
 
 	return {
-		isValid,
-		errorMessage: errorMessage
+		isValid: true,
+		errorMessage: null
 	};
 };
