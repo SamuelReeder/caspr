f313c4507797691c024c2ab145c4a5d95bf95608
Jaeyong Lee <jaeyonglee@geotab.com>
2024-11-25 20:30:08 -0500

    add dropdown for filters and sorting

--- frontend/src/components/GraphList.tsx
+++ frontend/src/components/GraphList.tsx
@@ -1,11 +1,11 @@
+import { Graph, GraphListProps } from "@/types/graph";
 /**
  * Graph list component
  * @param {Array} graphs - List of graphs
  * @param {String} page - Page title
  * @returns {ReactElement} Graph list component
  */
-import { Heading } from "@chakra-ui/react";
-import { Graph, GraphListProps } from "@/types/graph";
+import { Heading, Select, SelectProps } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 import ExploreGraphCard from "./ExploreGraphCard";
@@ -37,6 +37,7 @@ const GraphList = ({ graphs, page }: GraphListProps) => {
 						const owner = await getUser(graph.owner);
 						return { graph, owner };
 					} catch (error) {
+						console.error(error);
 						return { graph, owner: null };
 					}
 				})
@@ -48,11 +49,26 @@ const GraphList = ({ graphs, page }: GraphListProps) => {
 		fetchOwners();
 	}, [firestoreUser, graphs]);
 
+	const handleSelectChange: SelectProps["onChange"] = (event) => {
+		const selectedValue = event.target.value;
+		console.log(selectedValue);
+	};
+
 	return (
 		<div className="mt-8">
-			<Heading size="md" mb={4}>
-				{page}
-			</Heading>
+			<div className="flex flex-row">
+				<Heading size="md" mb={4}>
+					{page}
+				</Heading>
+
+				<div className="ml-auto">
+					<Select placeholder="Select option" onChange={handleSelectChange}>
+						<option value="option1">Option 1</option>
+						<option value="option2">Option 2</option>
+						<option value="option3">Option 3</option>
+					</Select>
+				</div>
+			</div>
 			{page === "Explore" ? (
 				<div className="flex flex-col gap-4">
 					{graphsWithOwners.map(({ graph, owner }, i) => (
