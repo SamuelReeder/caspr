4fc9d48401a4a3ec85c991c1506abd8caf37be90
Morris <morrisho215215@gmail.com>
2024-12-03 12:52:02 -0500

    Graphnavbar and graphsidebar test fix

--- frontend/__tests__/GraphSidebar.test.tsx
+++ frontend/__tests__/GraphSidebar.test.tsx
@@ -5,55 +5,65 @@ import GraphSideBar from "../src/components/GraphSideBar";
 import { NodeType, EdgeType } from "@/types";
 import data from "../src/data/100nodes_example.json"; // Import the JSON data
 import customRender from "@/test-utils/render";
+import { ViewProvider } from "@/context/ViewContext";
 
-describe("Simple Math", () => {
-  const mockNodes: NodeType[] = data.nodes;
-  const mockEdges: EdgeType[] = data.edges;
-  const mockOnNodeSelect = jest.fn();
+describe("Graph Sidebar Component", () => {
+	const mockNodes: NodeType[] = data.nodes;
+	const mockEdges: EdgeType[] = data.edges;
+	const mockOnNodeSelect = jest.fn();
 
-  const renderGraphSideBar = () => {
-    return customRender(
-      <GraphSideBar
-        nodes={mockNodes}
-        edges={mockEdges}
-        onNodeSelect={mockOnNodeSelect}
-      />
-    );
-  };
+	const renderGraphSideBar = () => {
+		return customRender(
+			<ViewProvider>
+				<GraphSideBar
+					nodes={mockNodes}
+					edges={mockEdges}
+					onNodeSelect={mockOnNodeSelect}
+				/>
+			</ViewProvider>
+		);
+	};
 
-  test("renders GraphSideBar component", () => {
-    renderGraphSideBar();
+	test("renders GraphSideBar component", () => {
+		renderGraphSideBar();
 
-    expect(screen.getByPlaceholderText("Search by label, id, or category...")).toBeInTheDocument();
-  });
-  
-  test("filters nodes based on search query", () => {
-    renderGraphSideBar();
+		expect(
+			screen.getByPlaceholderText("Search by label, id, or category...")
+		).toBeInTheDocument();
+	});
 
-    const searchInput = screen.getByPlaceholderText("Search by label, id, or category...");
-    fireEvent.change(searchInput, { target: { value: "Cybersecurity" } });
+	test("filters nodes based on search query", () => {
+		renderGraphSideBar();
 
-    expect(screen.getByText("Cybersecurity")).toBeInTheDocument();
+		const searchInput = screen.getByPlaceholderText(
+			"Search by label, id, or category..."
+		);
+		fireEvent.change(searchInput, { target: { value: "Cybersecurity" } });
 
-    mockNodes.forEach((node) => {
-      if (node.label !== "Cybersecurity") {
-        expect(screen.queryByText(node.label)).not.toBeInTheDocument();
-      }
-    });
-  });
+		expect(screen.getByText("Cybersecurity")).toBeInTheDocument();
 
-  test("handles node click", () => {
-    renderGraphSideBar();
+		mockNodes.forEach((node) => {
+			if (node.label !== "Cybersecurity") {
+				expect(screen.queryByText(node.label)).not.toBeInTheDocument();
+			}
+		});
+	});
 
-    const searchInput = screen.getByPlaceholderText("Search by label, id, or category...");
-    fireEvent.change(searchInput, { target: { value: "Cybersecurity" } });
+	test("handles node click", () => {
+		renderGraphSideBar();
 
-    const nodeElement = screen.getByText("Cybersecurity");
-    fireEvent.click(nodeElement);
+		const searchInput = screen.getByPlaceholderText(
+			"Search by label, id, or category..."
+		);
+		fireEvent.change(searchInput, { target: { value: "Cybersecurity" } });
 
-    const selectedNode = mockNodes.find((node) => node.label === "Cybersecurity");
+		const nodeElement = screen.getByText("Cybersecurity");
+		fireEvent.click(nodeElement);
 
-    expect(mockOnNodeSelect).toHaveBeenCalledWith(selectedNode);
-  });
+		const selectedNode = mockNodes.find(
+			(node) => node.label === "Cybersecurity"
+		);
+
+		expect(mockOnNodeSelect).toHaveBeenCalledWith(selectedNode);
+	});
 });
-
